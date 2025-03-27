4c8dfcbb92a993668e4eb596a19168530d8f164d
TYNIP <130399006+TYNIP@users.noreply.github.com>
2025-03-26 23:16:17 -0600

    LoRa Comunication | Success!

--- src/main.cpp
+++ src/main.cpp
@@ -1,65 +1,33 @@
-#include <SPI.h>
-#include <LoRa.h>
-
-// Módulo LoRa (SX1278)
-#define NSS 5    
-#define RST 14   
-#define DIO0 26  
+#include <Arduino.h>
 
 #ifdef TRANSMITTER
-  #define POT_PIN 34  
+#include "Transmitter.h"
+#endif
+
+#ifdef RECEIVER
+#include "Receiver.h"
 #endif
 
 void setup() {
-  Serial.begin(115200);
-  while (!Serial);  
+    Serial.begin(115200);
+    while (!Serial);
 
-  SPI.begin();
+    #ifdef TRANSMITTER
+    setupTransmitter();
+    #endif
 
-  LoRa.setPins(NSS, RST, DIO0);
-
-  Serial.println("Iniciando LoRa...");
-  if (!LoRa.begin(915E6)) {  
-    Serial.println("Error al iniciar LoRa");
-    while (1); 
-  }
-
-  #ifdef TRANSMITTER
-    Serial.println("Modo TRANSMISOR");
-  #endif
-
-  #ifdef RECEIVER
-    Serial.println("Modo RECEPTOR");
-  #endif
+    #ifdef RECEIVER
+    setupReceiver();
+    #endif
 }
 
 void loop() {
-  #ifdef TRANSMITTER
-    int potValue = analogRead(POT_PIN);
+    #ifdef TRANSMITTER
+    sendMessage("Esto esta encendido????");
+    delay(2000);
+    #endif
 
-    LoRa.beginPacket();
-    LoRa.write(potValue);  
-    LoRa.endPacket();
-
-    Serial.print("Valor del potenciómetro enviado: ");
-    Serial.println(potValue);
-
-    delay(1000);
-  #endif
-
-  #ifdef RECEIVER
-    int packetSize = LoRa.parsePacket();
-    Serial.println(packetSize);
-    if (packetSize) {
-      String received = "";
-      while (LoRa.available()) {
-        received += (char)LoRa.read();
-      }
-      Serial.print("Valor recibido: ");
-      Serial.println(received);
-    }
-
-    Serial.println("Receptor - Ayuda no jala :c");
-    delay(1000);
-  #endif
+    #ifdef RECEIVER
+    receiveMessage();
+    #endif
 }
